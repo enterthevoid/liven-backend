30203c02a3cae79db23892b5d2c327ce2557dd6d
Martin <byrlukmartin@gmail.com>
2021-03-12 16:08:48 +0200

    validation

--- api/models/work.js
+++ api/models/work.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const workSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  description: String,
-  photos: Object,
+  id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  photos: { type: Object, required: true },
 });
 
 module.exports = mongoose.model("Work", workSchema);
