c53048973f6e5c79b60dc2db12ffb69778c1f46f
Martin <byrlukmartin@gmail.com>
2021-03-12 16:08:38 +0200

    formatt responces

--- api/routes/works.js
+++ api/routes/works.js
@@ -6,9 +6,20 @@ const Work = require("../models/work");
 
 router.get("/", (req, res, next) => {
   Work.find()
+    .select("id name description photos")
     .exec()
     .then((docs) => {
-      res.status(200).json(docs);
+      const responce = {
+        totalItemCount: docs.length,
+        works: docs.map((doc) => ({
+          id: doc._id,
+          name: doc.name,
+          description: doc.description,
+          photos: doc.photos,
+        })),
+      };
+
+      res.status(200).json(responce);
     })
     .catch((err) => {
       res.status(500).json({ error: err });
@@ -17,7 +28,7 @@ router.get("/", (req, res, next) => {
 
 router.post("/", (req, res, next) => {
   const work = new Work({
-    _id: new mongoose.Types.ObjectId(),
+    id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     description: req.body.description,
     photos: req.body.photos,
@@ -31,8 +42,13 @@ router.post("/", (req, res, next) => {
     .catch((err) => console.log(err));
 
   res.status(201).json({
-    message: "Created",
-    work: work,
+    status: "Created",
+    work: {
+      id: work.id,
+      name: work.name,
+      description: work.description,
+      photos: work.photos,
+    },
   });
 });
 
@@ -40,10 +56,16 @@ router.get("/:workId", (req, res, next) => {
   const id = req.params.workId;
 
   Work.findById(id)
+    .select("id name description photos")
     .exec()
     .then((doc) => {
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+          id: doc._id,
+          name: doc.name,
+          description: doc.description,
+          photos: doc.photos,
+        });
       } else
         res
           .status(404)
@@ -64,8 +86,10 @@ router.patch("/:workId", (req, res, next) => {
 
   Work.update({ _id: id }, { $set: updateOps })
     .exec()
-    .then((result) => {
-      res.status(200).json(result);
+    .then(() => {
+      res.status(200).json({
+        status: "Created",
+      });
     })
     .catch((err) =>
       res.status(500).json({
@@ -79,7 +103,11 @@ router.delete("/:workId", (req, res, next) => {
 
   Work.remove({ _id: id })
     .exec()
-    .then((result) => res.status(200).json(result))
+    .then(() =>
+      res.status(200).json({
+        status: "Deleted",
+      })
+    )
     .catch((err) => res.status(500).json({ error: err }));
 });
 
