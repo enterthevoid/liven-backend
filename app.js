e626363f18689bd2054afd1a21bd7973bdf5d196
Martin <byrlukmartin@gmail.com>
2021-05-31 18:19:34 +0300

    minor

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-requested-With, Content-Type, Accept, Authorization"
+    "x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *"
   );
 
   if (req.method === "OPTIONS") {
